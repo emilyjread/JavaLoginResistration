2a3104ba3912e428685c44e465154d4cfe0222d6
Emily Read <emilyread01@gmail.com>
2021-08-16 15:42:40 -0700

    added index functionality

--- src/main/java/com/emilyread/authentication/controllers/MainController.java
+++ src/main/java/com/emilyread/authentication/controllers/MainController.java
@@ -13,20 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.emilyread.authentication.models.User;
 import com.emilyread.authentication.services.UserService;
+import com.emilyread.authentication.validator.UserValidator;
 
 @Controller
 public class MainController {
 	
 	private final UserService userService;
+	private final UserValidator userValidator;
 	
-	public MainController(UserService userService) {
+	public MainController(UserService userService, UserValidator userValidator) {
+		this.userValidator= userValidator;
 		this.userService = userService;
 	}
 	
 	@RequestMapping("/home")
 	public String home(@ModelAttribute("user") User user, Model model,  HttpSession session) {
-		model.addAttribute(user);
-		session.getAttribute("user");
+
+		User u = (User) session.getAttribute("user");
+		model.addAttribute("user", u);
 		return "index.jsp";
 	}
 
@@ -37,13 +41,13 @@ public class MainController {
 	
 	@RequestMapping(value="/registration",  method=RequestMethod.POST)
 	public String register(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
-		
+		userValidator.validate(user, result);
 		if(result.hasErrors()) {
 			return "/register.jsp";
 		}
 		else {
 			User u = userService.registerUser(user);
-			session.setAttribute("user", u.getEmail());
+			session.setAttribute("user", u);
 			return "redirect:/home";
 		}
 	}
@@ -54,13 +58,15 @@ public class MainController {
 	}
 	
 	@RequestMapping(value="/login",  method=RequestMethod.POST)
-	public String login(@Valid @ModelAttribute("user") User user, BindingResult result, @RequestParam("email") String email, @RequestParam("password") String password){
+	public String login(@Valid @ModelAttribute("user") User user, BindingResult result, @RequestParam("email") String email, @RequestParam("password") String password, HttpSession session){
 		
 		if(result.hasErrors()) {
 			return "/login.jsp";
 		}
 		else {
 			userService.authenticateUser(email, password);
+			User u = userService.registerUser(user);
+			session.setAttribute("user", u);
 			return "redirect:/home";
 		}
 	}
